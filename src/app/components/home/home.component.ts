511d48901d181604f0e2626b244d43679e69eeea
Samuel Ricardo <samuelric4rdo@gmail.com>
2025-06-26 15:02:07 -0300

    adicao de lupa e input de pesquisa

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -5,28 +5,34 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
-
 import { HomeService } from '../../services/home.service';
 import { Produtos } from '../../types/produtos.types';
+import { MatFormField } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
+    FormsModule,
     CommonModule,
     MatCardModule,
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
-    MatDividerModule
+    MatDividerModule,
+    MatFormField,
+    MatInputModule
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent implements OnInit {
   produtos: Produtos[] = [];
+  produtosOriginais: Produtos[] = [];
 
-  constructor(private homeService: HomeService) {}
+  constructor(private homeService: HomeService) { }
 
   ngOnInit(): void {
     this.carregarProdutos();
@@ -34,8 +40,30 @@ export class HomeComponent implements OnInit {
 
   carregarProdutos(): void {
     this.homeService.getProdutosEmDestaque().subscribe((produtos) => {
+      this.produtosOriginais = produtos;
       this.produtos = produtos;
       console.log(produtos)
     });
+  }
+
+  searchTerm = '';
+
+  onSearchChange() {
+    const termo = this.searchTerm.trim().toLowerCase();
+
+    if (termo === '') {
+      this.produtos = this.produtosOriginais;
+    } else {
+      this.produtos = this.produtosOriginais.filter(p => 
+        p.nome.toLowerCase().includes(termo)
+      );
+    }
+
+    console.log('Buscar por:', termo, this.produtos);
+  }
+
+  clearSearch() {
+    this.searchTerm = '';
+    this.onSearchChange();
   }
 }
